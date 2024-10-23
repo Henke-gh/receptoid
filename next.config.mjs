fa1ea4f6674f0f37619c5c154dcf5134aaef67b4
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-10-23 09:28:36 +0200

    environment setup

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+/* const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // Next.js config options here
+}); */
+
+export default nextConfig;
